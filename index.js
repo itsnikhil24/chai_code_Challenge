debfedf50545bdfd3816cf66a44ad9ebf87ae524
Nikhil Sharma <nikhils.sharma2409@gmail.com>
2024-07-24 00:04:01 +0530

    Data Types learning

---
+++ index.js
@@ -0,0 +1,37 @@
+// Activity 1
+
+var a=100;
+console.log(a);
+
+
+let names ="Nikhil";
+console.log(names);
+
+
+const bool=2>3;
+console.log(bool);
+
+
+let x=100;
+let y="nikhil";
+let z=500>2;
+let arr=[1,2,3,4,5];
+
+let obj={
+    name : "Nikhil",
+    age:18,
+    height : 5000
+
+}
+console.log(typeof(x),typeof(y),typeof(z),typeof(arr),typeof(obj));
+
+
+let val1=200;
+console.log(val1);
+val1=300;
+console.log(val1);
+
+const val2=100;
+console.log(val2);
+// val2=200;
+// console.log(val2);
